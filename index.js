c5c4fe01cddd19c8dd9be5879314168571615068
Lasko Laskov <laskov.lasko@gmail.com>
2020-12-22 19:41:39 +0200

    added file logger

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const {
     EventEmitter
 } = require('events')
 const fetch = require('node-fetch')
+const SimpleNodeLogger = require('simple-node-logger')
+const logOpts = {
+    logFilePath: 'lichess-playground.log',
+    timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
+}
+const log = SimpleNodeLogger.createSimpleFileLogger(logOpts)
 const readline = require('readline')/* .createInterface({
     input: process.stdin,
     output: process.stdout
@@ -22,71 +28,71 @@ let currentGame = null
 let isWhite = null
 
 async function processEvent(e) {
-    console.log(`Processing event: ${e.type}`)
-    console.log(e)
+    log.info(`Processing event: ${e.type}`)
+    log.info(e)
     switch (e.type) {
         case 'challenge':
             processChallenge(e)
             return
         case 'challengeCanceled':
-            console.log(`Challenge by ${e.challenge.challenger.name} was canceled.`)
+            log.info(`Challenge by ${e.challenge.challenger.name} was canceled.`)
             return
         case 'challengeDeclined':
-            console.log(`Challenge to ${e.challenge.challenger.name} was declined.`)
+            log.info(`Challenge to ${e.challenge.challenger.name} was declined.`)
             return
         case 'gameStart':
-            console.log(`Starting game '${e.game.id}'.`)
+            log.info(`Starting game '${e.game.id}'.`)
             currentGame = e.game.id
             gameState(currentGame)
             return
         case 'gameFinish':
             if (e.game.id === currentGame) {
-                console.log(`Ended current game (${currentGame}).`)
+                log.info(`Ended current game (${currentGame}).`)
                 currentGame = null
             }
             return
         default:
-            console.log(`Unknown event '${e.type}'.`)
+            log.info(`Unknown event '${e.type}'.`)
             return
     }
 }
 
 async function processGameState(e) {
-    console.log(`Processing game event: ${e.type}`)
-    console.log(e)
+    log.info(`Processing game event: ${e.type}`)
+    log.info(e)
     let movesMod = null
     switch (e.type) {
         case 'gameFull':
             isWhite = e.white.id === 'laskolaskov' ? true : false
             movesMod = e.state.moves.toString().split(' ').length % 2
-            console.log('mod :: ', movesMod)
-            if((isWhite && movesMod === 0) || (!isWhite && movesMod === 1)) {
+            log.info('mod :: ', movesMod)
+            if ((isWhite && movesMod === 0) || (!isWhite && movesMod === 1)) {
                 makeMove()
             }
             return
         case 'gameState':
             //status: started | aborted | resign
-            if(e.status !== 'started') {
+            if (e.status !== 'started') {
                 return
             }
             movesMod = e.moves.toString().split(' ').length % 2
-            console.log('mod :: ', movesMod)
-            if((isWhite && movesMod === 0) || (!isWhite && movesMod === 1)) {
+            log.info('mod :: ', movesMod)
+            if ((isWhite && movesMod === 0) || (!isWhite && movesMod === 1)) {
                 makeMove()
             }
             return
         case 'chatLine':
-            //console.log(`Challenge to ${e.challenge.challenger.name} was declined.`)
+            //log.info(`Challenge to ${e.challenge.challenger.name} was declined.`)
             return
         default:
-            console.log(`Unknown event '${e.type}'.`)
+            log.info(`Unknown event '${e.type}'.`)
             return
     }
 }
 
 async function processChallenge(e) {
     if (currentGame) {
-        console.log(`Game ${currentGame} in progress. Auto declining challenge (${e.challenge.id}) by ${e.challenge.challenger.name}.`)
+        log.info(`Game ${currentGame} in progress. Auto declining challenge (${e.challenge.id}) by ${e.challenge.challenger.name}.`)
         declineChallenge(e.challenge.id)
         return
     }
@@ -99,7 +105,7 @@ async function processChallenge(e) {
     rl.setPrompt(`Do you want to accept challenge by ${e.challenge.challenger.name} ? (y/n): `)
     rl.on('line', async (line) => {
         const answer = line.trim()
-        console.log('answer :: ', answer)
+        log.info('answer :: ', answer)
         if (answer === 'y') {
             rl.close()
             acceptChallenge(e.challenge.id)
@@ -107,7 +113,7 @@ async function processChallenge(e) {
             rl.close()
             declineChallenge(e.challenge.id)
         } else {
-            console.log('Invalid answer.')
+            log.info('Invalid answer.')
             rl.prompt()
         }
     })
@@ -120,15 +126,15 @@ function makeMove() {
     rl.setPrompt(`Your move: `)
     rl.on('line', async (line) => {
         const move = line.trim()
-        console.log('move :: ', move)
+        log.info('move :: ', move)
         if (true /* TODO validate the move*/) {
             rl.close()
             //make move request
             const moveUrl = `${url}/api/board/game/${currentGame}/move/${move}`
             const result = await fetchUrl(moveUrl, 'POST')
-            result ? console.log(`Move '${move}' was made.`) : console.log(`Error while making move '${move}'.`)
+            result ? log.info(`Move '${move}' was made.`) : log.error(`Error while making move '${move}'.`)
         } else {
-            console.log('Invalid move.')
+            log.error('Invalid move.')
             rl.prompt()
         }
     })
@@ -146,16 +152,16 @@ function gameState(id) {
 
 async function acceptChallenge(id) {
     const acceptUrl = `https://lichess.org/api/challenge/${id}/accept`
-    console.log(`Accepting challenge '${id}'.`)
+    log.info(`Accepting challenge '${id}'.`)
     const result = await fetchUrl(acceptUrl, 'POST')
-    result ? console.log(`Challenge '${id}' accepted.`) : console.log(`Error while accepting challenge '${id}'.`)
+    result ? log.info(`Challenge '${id}' accepted.`) : log.error(`Error while accepting challenge '${id}'.`)
 }
 
 async function declineChallenge(id) {
     const declineUrl = `https://lichess.org/api/challenge/${id}/decline`
-    console.log(`Declining challenge ${id}.`)
+    log.info(`Declining challenge ${id}.`)
     const result = await fetchUrl(declineUrl, 'POST')
-    result ? console.log(`Challenge '${id}' declined.`) : console.log(`Error while declining challenge '${id}'.`)
+    result ? log.info(`Challenge '${id}' declined.`) : log.error(`Error while declining challenge '${id}'.`)
     //clear if declining current challenge
     if (currentGame === id) {
         currentGame = null
@@ -172,7 +178,7 @@ async function fetchUrl(url, method = 'GET') {
 
     while (true) {
         try {
-            console.log(`Calling: ${url}`)
+            log.info(`Calling: ${url}`)
             const resp = await fetch(url, {
                 method,
                 headers: {
@@ -183,16 +189,16 @@ async function fetchUrl(url, method = 'GET') {
                 const message = await resp.text()
                 throw new Error(`${resp.status}: ${message}`)
             }
-            console.log(`success`)
+            log.info(`success`)
             return true
         } catch (error) {
-            console.log(error)
+            log.error(error)
             retry++
             if (retry >= maxAttempts) {
-                console.log(`Maximum attempts (${maxAttempts}) reached. Aborting.`)
+                log.warn();(`Maximum attempts (${maxAttempts}) reached. Aborting.`)
                 return false
             }
-            console.log(`Trying again! Attempt: ${retry + 1}.`)
+            log.warn(`Trying again! Attempt: ${retry + 1}.`)
         }
     }
 }
@@ -208,7 +214,7 @@ function streamUrl(url) {
 
         while (true) {
             try {
-                console.log(`Calling: ${url}`)
+                log.info(`Calling: ${url}`)
                 const resp = await fetch(url, {
                     headers: {
                         'Authorization': `Bearer ${token}`
@@ -218,7 +224,7 @@ function streamUrl(url) {
                     const message = await resp.text()
                     throw new Error(`${resp.status}: ${message}`)
                 }
-                console.log('Connected!')
+                log.info('Connected!')
                 resp.body.on('data', async (chunk) => {
                     const data = chunk.toString()
                     if (data.trim()) {
@@ -227,13 +233,13 @@ function streamUrl(url) {
                 })
                 break
             } catch (error) {
-                console.log(error)
+                log.error(error)
                 retry++
                 if (retry >= maxAttempts) {
-                    console.log(`Maximum attempts (${maxAttempts}) reached. Aborting.`)
+                    log.warn(`Maximum attempts (${maxAttempts}) reached. Aborting.`)
                     break
                 }
-                console.log(`Trying again! Attempt: ${retry + 1}.`)
+                log.warn(`Trying again! Attempt: ${retry + 1}.`)
             }
         }
     })()
